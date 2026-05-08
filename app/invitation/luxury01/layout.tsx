f3fcc0cc6a39e37067a9e2bcfabdc19f8de058d1
Ican09 <kaisanfarasdag09@gmail.com>
2026-05-08 22:11:18 +0800

    update route layout metadata

--- app/invitation/luxury01/layout.tsx
+++ app/invitation/luxury01/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://raha-invitation.vercel.app"),
-
   title: "The Wedding of Dillo & Alisyah",
   description: "Wedding Invitation",
 
@@ -11,17 +9,14 @@ export const metadata: Metadata = {
     description: "Sabtu, 18 Mei 2026",
     url: "/invitation/luxury01",
     siteName: "Raha Invitation",
-
     images: [
       {
-        url: "/images/luxury01/wedding-preview.jpg",
+        url: "/images/luxury01/og-image.jpg",
         width: 1200,
         height: 630,
         alt: "Wedding Invitation",
-        type: "image/jpeg",
       },
     ],
-
     locale: "id_ID",
     type: "website",
   },
@@ -30,7 +25,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "The Wedding of Dillo & Alisyah",
     description: "Sabtu, 18 Mei 2026",
-    images: ["/images/luxury01/wedding-preview.jpg"],
+    images: ["/images/luxury01/og-image.jpg"],
   },
 };
 
